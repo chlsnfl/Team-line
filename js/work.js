923f9966431e8f3d46c261108ecb2e8daa1e1e64
myeongjin <125331795+myeongjin123@users.noreply.github.com>
2023-04-18 20:01:08 +0900

    sum

--- js/work.js
+++ js/work.js
@@ -3,3 +3,20 @@ $(function(){
         $('.lnb').stop().slideToggle();
     });
 });
+
+/******************명진 ****************** */
+$(function(){
+    setInterval(mySlide, 3000);
+    
+        function mySlide(){
+           $('.slidein').animate({
+               'left': '-1200px'
+           }, 500, function(){
+              $('.slidein img:first-child')
+              .clone()
+              .appendTo('.slidein');
+              $('.slidein img:first-child').remove();
+              $('.slidein').css('left', 0);
+           });
+        }
+    });
